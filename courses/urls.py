cb140e868a2e4e155c4d672b8a96298e96809d57
lowryel <eugeneagbaglo@gmail.com>
2022-01-07 16:27:25 +0000

    Subscription app with options for free, enterprise and professional. The techs used are html, bootstrap, python anad django with stripe api integration

---
+++ courses/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from courses.views import *
+app_name = 'courses'
+
+urlpatterns = [
+    path('', CourseListView.as_view(), name='list'),
+    path('<slug>', CourseDetailView.as_view(), name='detail'),
+    path('<course_slug>/<lesson_slug>', LessonDetailView.as_view(), name='lesson-detail'),
+]
